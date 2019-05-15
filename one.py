5d1c5bcd3dda4dd8d7eb8f36cfba4ce440b3e6a0
eric-Is-Me <echai009@ucr.edu>
2019-05-15 16:08:27 -0700

    part one finished

---
+++ one.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+import re
+
+rejects = 0
+quarantines = 0
+minute = "00"
+thisLine = []
+
+with open(str(sys.argv[1]), 'r') as f:
+	for line in f:
+		thisLine = re.split(':|,|->| ', line)
+		if minute != thisLine[4]:
+			outFile = open("hourlyInfo.txt", 'a')
+			outFile.write("Mar 1 00:" + str(minute) + " [postfix rejects:" + str(rejects) + "] [amavis quarantines:" + str(quarantines) + "]\n")
+			rejects = 0
+			quarantines = 0
+		minute = thisLine[4]
+		if "reject" in line:
+			rejects += 1
+		if "quarantine" in line:
+			quarantines += 1
+outFile.close()
+f.close
