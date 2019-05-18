7cd2eac5525504e4975b472609ce6e4d312b4d20
Eric <echai009@ucr.edu>
2019-05-18 16:33:05 -0700

    part 2 finished

---
+++ three.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.6
+import re
+s = "Mar 1 00:00:05 avas postfix/qmgr[23698]: 445C261AA4: from=<root@example.com>, size=04, nrcpt=1 (queue active)"
+
--- two.py
+++ two.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3.6
 import re
-s = "mar 1 00:30:03 avas postfix/smtpd[2610]: connect from unknown[208.37.192.234]"
-out = re.split(r'\[|\]', s)
-print(out)
+from statistics import mode
+from collections import Counter
+
+unknowns = []
+knowns = []
 
 with open('log2', 'r') as f:
 	for line in f:
 		if  (re.search(' connect from ', line) and re.search('unknown', line)):
-	
+			line = re.split(r'\[|\]', line)
+			unknowns.append(line[-2])
+		elif re.search(' connect from ', line):
+			line = re.split(r'\[|\]', line)
+			knowns.append(line[-2])
+	numKnown = len(knowns)
+	numUnknown = len(unknowns)
+	commonKnown = mode(knowns)
+	commonUnknown = mode(unknowns)
+	numCom = knowns.count(commonKnown)
+	numUCom = unknowns.count(commonUnknown)
+	print("Total Unknown connections: {} - [{}] for {} connections".format(numUnknown, commonUnknown, numUCom))
+	print("Total Known connections: {} - [{}] for {} connections".format(numKnown, commonKnown, numCom))
