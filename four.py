e13f84cd752931bbd6e1d563542496bde8030056
Eric <echai009@ucr.edu>
2019-05-18 19:59:00 -0700

    part 4 finished

--- four.py
+++ four.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3.6
 import re
+import numpy as np
+
+ip = []
+received = []
+sent = []
+rejects = 0
+
+with open('log4', 'r') as f:
+	for line in f:
+		if re.search('blocked using dnsbl.sorbs.net', line):
+			split = re.split(';', line)
+			ipString = re.split(' |\[|\]', split[1])
+			ip.append(ipString[4])
+			temp = re.search('from=<(\w+[\W+|.|\w])*@(\w+[.])*([\w\W])*>', split[-1])
+			temp2 = re.search('to=<(\w+[\W+|.|\w])*@(\w+[.])*([\w\W])*>', split[-1])
+			received.append(temp.group(0))
+			sent.append(temp2.group(0))
+			rejects += 1
+	ipArr = np.array(ip)
+	receivedArr = np.array(received)
+	sentArr = np.array(sent)
+	print("{} messages rejected".format(rejects))
+	print("{} unique IP's".format(len(np.unique(ipArr))))
+	print("{} unique from addresses".format(len(np.unique(receivedArr))))
+	print("{} unique to addresses".format(len(np.unique(sentArr))))
